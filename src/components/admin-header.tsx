4bee79205ed3de527a898c43db2c0237667c851f
Paulo Grochowski <paulogrochowski@gmail.com>
2025-07-05 06:01:35 +0000

    Botao para recolher o meni

--- src/components/admin-header.tsx
+++ src/components/admin-header.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import { useSidebar, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { User, LogOut } from "lucide-react";
 import { ThemeToggle } from "./theme-toggle";
 
 export function AdminHeader() {
-    const { isMobile } = useSidebar();
-
     return (
         <header className="sticky top-0 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-            {isMobile && <SidebarTrigger />}
+            <SidebarTrigger />
             <div className="relative ml-auto flex-1 md:grow-0">
                 {/* Maybe a search bar later */}
             </div>
