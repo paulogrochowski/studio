472fd56b4f1e489f6b1e4c07a96ad1159f17646d
Paulo Grochowski <paulogrochowski@gmail.com>
2025-07-08 13:31:38 +0000

    Adicionar um copinho rodando nas páginas de loading

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -7,7 +7,7 @@ interface LoaderProps {
 export function Loader({ message = "Processando..." }: LoaderProps) {
   return (
     <div className="flex flex-col items-center justify-center gap-4 p-8 rounded-lg text-card-foreground w-full">
-      <Icons.logo className="w-12 h-12 text-primary animate-spin" />
+      <Icons.cup className="w-12 h-12 text-primary animate-spin" />
       <div className="text-center">
         <p className="text-muted-foreground mt-2">{message}</p>
       </div>
